621229689ba290f5747609cbf529d84962d312ad
Carter Reeb <me@carteris.online>
2024-09-04 21:23:49 -0400

    enable cachix community cache

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,15 @@
       ./subconf/networking.nix
     ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   hardware.bluetooth.enable = true;
