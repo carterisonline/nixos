f64c6b3e4f8648030b4f8ef3884459eb7539595d
Carter Reeb <me@carteris.online>
2024-09-05 10:43:52 -0400

    actually dont do the archwiki smooth transition hack

--- subconf/plymouth.nix
+++ subconf/plymouth.nix
@@ -1,4 +1,4 @@
-{ pkgs, stylix, ...}:
+{ pkgs, ...}:
 
 {
   boot.plymouth = {
@@ -21,25 +21,6 @@
     "rd.systemd.show_status=false"
     "rd.udev.log_level=3"
     "udev.log_priority=3"
-  ];
-
-  systemd.packages = [
-    (pkgs.runCommandNoCC "plymouth.conf" {
-      preferLocalBuild = true;
-      allowSubstitutes = false;
-    } ''
-      mkdir -p $out/etc/systemd/system/display-manager.service.d/
-      cat <<EOF > $out/etc/systemd/system/display-manager.service.d/plymouth.conf
-        [Unit]
-        Conflicts=plymouth-quit.service
-        After=plymouth-quit.service rc-local.service plymouth-start.service systemd-user-sessions.service
-        OnFailure=plymouth-quit.service
-
-        [Service]
-        ExecStartPost=-/usr/bin/sleep 30
-        ExecStartPost=-/usr/bin/plymouth quit --retain-splash
-      EOF
-    '')
   ];
 
   stylix.targets.plymouth.enable = false;
