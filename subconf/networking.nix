caeba25cb45241e407276e5eb4f05874142cb866
Carter Reeb <me@carteris.online>
2024-09-05 10:25:03 -0400

    disable secure dns

--- subconf/networking.nix
+++ subconf/networking.nix
@@ -3,16 +3,16 @@
     hostName = "nixos";
     networkmanager = {
       enable = true;
-      dns = "none";
+      # dns = "none";
       wifi.powersave = true;
     };
 
-    dhcpcd.extraConfig = "nohook resolv.conf";
+    #dhcpcd.extraConfig = "nohook resolv.conf";
     
-    nameservers = [
-      "127.0.0.1"
-      "::1"
-    ];
+    # nameservers = [
+    #   "127.0.0.1"
+    #   "::1"
+    # ];
 
     nftables.enable = true;
     firewall = { 
@@ -26,25 +26,25 @@
     };
   };
 
-  services.dnscrypt-proxy2 = {
-    enable = true;
-    settings = {
-      ipv6_servers = true;
-      block_ipv6 = false;
+  # services.dnscrypt-proxy2 = {
+  #   enable = true;
+  #   settings = {
+  #     ipv6_servers = true;
+  #     block_ipv6 = false;
 
-      require_dnssec = true;
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-      };
-    };
-  };
+  #     require_dnssec = true;
+  #     sources.public-resolvers = {
+  #       urls = [
+  #         "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+  #         "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+  #       ];
+  #       cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
+  #       minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+  #     };
+  #   };
+  # };
 
-  systemd.services.dnscrypt-proxy2.serviceConfig = {
-    StateDirectory = "dnscrypt-proxy";
-  };
+  # systemd.services.dnscrypt-proxy2.serviceConfig = {
+  #   StateDirectory = "dnscrypt-proxy";
+  # };
 }
