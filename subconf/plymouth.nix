f02a44f97633713fcffd26d8a5dfb98374a99fd2
Carter Reeb <me@carteris.online>
2024-09-05 10:31:46 -0400

    enable smooth plymouth transition into sddm

--- subconf/plymouth.nix
+++ subconf/plymouth.nix
@@ -1,3 +1,5 @@
+{ pkgs, ...}:
+
 {
   boot.plymouth.enable = true;
   boot.consoleLogLevel = 0;
@@ -10,5 +12,24 @@
     "rd.systemd.show_status=false"
     "rd.udev.log_level=3"
     "udev.log_priority=3"
+  ];
+
+  systemd.packages = [
+    (pkgs.runCommandNoCC "plymouth.conf" {
+      preferLocalBuild = true;
+      allowSubstitutes = false;
+    } ''
+      mkdir -p $out/etc/systemd/system/display-manager.service.d/
+      cat <<EOF > $out/etc/systemd/system/display-manager.service.d/plymouth.conf
+        [Unit]
+        Conflicts=plymouth-quit.service
+        After=plymouth-quit.service rc-local.service plymouth-start.service systemd-user-sessions.service
+        OnFailure=plymouth-quit.service
+
+        [Service]
+        ExecStartPost=-/usr/bin/sleep 30
+        ExecStartPost=-/usr/bin/plymouth quit --retain-splash
+      EOF
+    '')
   ];
 }
