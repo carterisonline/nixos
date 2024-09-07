8a16781e6509dfd83ba11dbe43379b6936043b07
Carter Reeb <me@carteris.online>
2024-09-06 20:21:19 -0400

    switch back to 6.10

--- configuration.nix
+++ configuration.nix
@@ -35,7 +35,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_10;
 
   boot.supportedFilesystems = [ "bcachefs" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
