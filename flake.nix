d2501c1715be4a6beb25d7883ea4b71122d28bfa
Carter Reeb <me@carteris.online>
2025-03-07 17:07:05 -0500

    full system update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     bitwig-studio.url = "github:NixOS/nixpkgs/05bbf675397d5366259409139039af8077d695ce";
     
-    stylix.url = "github:danth/stylix/release-24.11";
+    stylix.url = "github:danth/stylix";
+    stylix.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     flatpaks.url = "github:GermanBread/declarative-flatpak/stable-v3";
