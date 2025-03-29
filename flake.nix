30c5c549d1b66ca8eea07ea81b19640c9eb5545f
Carter Reeb <me@carteris.online>
2025-03-28 21:56:34 -0400

    add dolphin-emu

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
         (pkgFromFlake [ "nixGLIntel" "nixVulkanIntel" ] nixGL)
         (pkgFromFlake isd)
         (pkgOverlay [ "bitwig-studio" ] bitwig-studio)
-        (pkgOverlay [ "archipelago" "plugdata" "zed-editor" ] nixpkgs-unstable)
+        (pkgOverlay [ "archipelago" "dolphin-emu" "plugdata" "zed-editor" ] nixpkgs-unstable)
                 
         stylix.nixosModules.stylix
         home-manager.nixosModules.home-manager {
