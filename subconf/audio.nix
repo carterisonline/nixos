1872546840f1e37a245e598ea8d29963c15984dd
Carter Reeb <me@carteris.online>
2026-02-11 20:56:49 -0500

    (boot) allow audio group to configure latency variables

--- subconf/audio.nix
+++ subconf/audio.nix
@@ -23,4 +23,10 @@
     { domain = "@audio"; item = "nofile"; type = "soft"; value = "99999"; }
     { domain = "@audio"; item = "nofile"; type = "hard"; value = "99999"; }
   ];
+
+  services.udev.extraRules = ''
+    KERNEL=="rtc0", GROUP="audio"
+    KERNEL=="hpet", GROUP="audio"
+    DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
+  '';
 }
