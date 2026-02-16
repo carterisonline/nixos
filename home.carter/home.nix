7acee19bbac7b317cc232fb00403becea9545bbd
Carter Reeb <me@carteris.online>
2026-02-16 01:28:09 -0500

    (home) remove duplicate helix program config

--- home.carter/home.nix
+++ home.carter/home.nix
@@ -73,23 +73,6 @@
     };
   };
 
-  programs.helix = {
-    enable = true;
-    settings = {
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
-      };
-      editor.lsp.display-messages = true;
-    };
-    languages.language = [{
-      name = "nix";
-      auto-format = true;
-      formatter.command = "${pkgs.alejandra}/bin/alejandra";
-    }];
-  };
-
   programs.kitty = {
     enable = true;
     shellIntegration.enableFishIntegration = true;
