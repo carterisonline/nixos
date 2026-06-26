6975b600758c3577a3e939bdcf65a2ff511f07e9
Carter Reeb <me@carteris.online>
2026-06-26 17:34:28 -0400

    (runtime) adjust sysctl params from cachyos

--- subconf/boot.nix
+++ subconf/boot.nix
@@ -12,7 +12,11 @@
     kernelParams = [ "threadirqs" "preempt=full" "nohz_full=all" ];
 
     kernel.sysctl = {
-      "kernel.sysrq" = 1;
+      "kernel.nmi_watchdog" = 0;
+      "vm.dirty_background_bytes" = 67108864;
+      "vm.dirty_bytes" = 268435456;
+      "vm.dirty_writeback_centisecs" = 1500;
+      "vm.vfs_cache_pressure" = 50;
     };
     
     supportedFilesystems = [ "bcachefs" ];
